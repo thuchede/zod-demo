a35d8eb7d02ef9a7b1019a4e1d57bf1b064e0d7c
Thomas Huchedé <huchede.thomas@gmail.com>
2023-10-05 16:29:26 +0800

    Initial commit

---
+++ apps/geekcamp-login/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="geekcamp-login" />
+    </div>
+  );
+}
+
+export default App;
