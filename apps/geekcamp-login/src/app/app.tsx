27cac37effb38ca71cc4505e87f2412ca4cc730d
Thomas Huchedé <huchede.thomas@gmail.com>
2023-10-14 13:30:06 +0800

    step 2: write custom validation

--- apps/geekcamp-login/src/app/app.tsx
+++ apps/geekcamp-login/src/app/app.tsx
@@ -1,9 +1,14 @@
 import { FormEvent, useState } from 'react';
 
 const PASSWORD_PATTERN =
-  '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$';
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$/;
 const PASSWORD_HINT =
   'Need at least 1 lowercase, 1 uppercase, 1 number, 1 special char';
+
+type SignUpFormData = {
+  email?: string;
+  password?: string;
+};
 
 function getFormValues(e: FormEvent<HTMLFormElement>): unknown {
   return Array.from<{ name: string; value: string }>(
@@ -18,11 +23,37 @@ function getFormValues(e: FormEvent<HTMLFormElement>): unknown {
 
 export function App() {
   const [registered, setRegistered] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<string | undefined>(undefined);
+  const [passwordError, setPasswordError] = useState<string | undefined>(
+    undefined
+  );
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formValues = getFormValues(e);
+    const formValues = getFormValues(e) as SignUpFormData;
+
+    let hasError = false;
+
+    if (!formValues.email) {
+      setEmailError('Required');
+      hasError ||= true;
+    } else if (!formValues.email.includes('@')) {
+      setEmailError('Invalid email');
+      hasError ||= true;
+    }
+
+    if (!formValues.password) {
+      setPasswordError('Required');
+      hasError ||= true;
+    } else if (!PASSWORD_PATTERN.test(formValues.password)) {
+      setPasswordError('Too easy');
+      hasError ||= true;
+    }
+
+    if (hasError) {
+      return;
+    }
 
     fetch('http://localhost:3333/signup', {
       method: 'POST',
@@ -58,6 +89,7 @@ export function App() {
 
       <form
         onSubmit={handleSubmit}
+        noValidate
         className={
           'flex flex-col gap-6 mx-auto max-w-md p-8 rounded-lg bg-gray-100 shadow-lg'
         }
@@ -70,10 +102,14 @@ export function App() {
             type="email"
             id={'email'}
             name={'email'}
-            required
+            aria-required
+            onChange={() => setEmailError(undefined)}
             placeholder={'john.doe@example.com'}
             className={'py-1 px-2'}
           />
+          <div className={'h-6'}>
+            {emailError && <p className="text-red-400">{emailError}</p>}
+          </div>
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="password" className={'flex flex-col gap-1'}>
@@ -84,12 +120,13 @@ export function App() {
             id={'password'}
             name={'password'}
             required
-            minLength={5}
-            pattern={PASSWORD_PATTERN}
-            title={PASSWORD_HINT}
+            onChange={() => setPasswordError(undefined)}
             placeholder={'••••••••'}
             className={'py-1 px-2'}
           />
+          <div className={'h-6'}>
+            {passwordError && <p className="text-red-400">{passwordError}</p>}
+          </div>
         </div>
         <button
           className={
