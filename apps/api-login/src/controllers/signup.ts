2a33bb99d2e8e9580770ae92e4b0ca646c2b82d5
Thomas Huchedé <huchede.thomas@gmail.com>
2023-10-14 13:37:40 +0800

    step 3.1: use zod for back-end validation

--- apps/api-login/src/controllers/signup.ts
+++ apps/api-login/src/controllers/signup.ts
@@ -1,9 +1,23 @@
 import { Request, Response, Router } from 'express';
+import { z } from 'zod';
 
 export const signupController = Router();
 
+const PASSWORD_PATTERN =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$/;
+
+const SignUpFormData = z.object({
+  email: z.string().min(1, 'Required').email('Invalid email'),
+  password: z.string().min(1, 'Required').regex(PASSWORD_PATTERN, 'Too easy'),
+});
+
 function signupHandler(req: Request, res: Response) {
   // validation should also happen here
+  try {
+    SignUpFormData.parse(req.body);
+  } catch (e) {
+    return res.status(422).send(e.issues);
+  }
   // then processing/db storage
   return res.status(201).send();
 }
